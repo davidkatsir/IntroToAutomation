7a0bedceaebdc20492f3e7b65a1a76f47dfcc1b1
David Katsir <david.katsir@gmail.com>
2024-05-28 15:32:57 +0300

    Commit Add FizBuz Problem Description

--- src/lessonTwo/FizBazFizBaz.java
+++ src/lessonTwo/FizBazFizBaz.java
@@ -1,5 +1,16 @@
 package lessonTwo;
 
+//Problem Description
+//You need to write a program that prints the numbers from 1 to a given number `n`.
+// However, for multiples of 3, print "Fiz" instead of the number,
+// for multiples of 5, print "Baz" instead of the number,
+// and for multiples of both 3 and 5, print "FizBaz".
+
+// * This problem helps to test basic programming skills, including loops, conditionals,
+// and modulus operations. It also tests your ability to follow specifications
+// and handle edge cases, such as different ranges of `n`.
+
+
 public class FizBazFizBaz {
     public static void main(String[] args) {
         int n = 20; // Example usage
